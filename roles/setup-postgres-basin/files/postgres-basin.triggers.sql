a9cf01d087eda3f228ec7a239d80f19c02c6c03f
Michael Lustenberger <mic@inofix.ch>
2025-07-16 02:58:04 +0200

    Consolidate Basin SQL: Trigger
    Unify, useing newer syntax.

--- roles/setup-postgres-basin/files/postgres-basin.triggers.sql
+++ roles/setup-postgres-basin/files/postgres-basin.triggers.sql
@@ -4,7 +4,8 @@
 -- ## TABLE projects
 CREATE OR REPLACE TRIGGER insert_project_post_process
 AFTER INSERT ON projects
-FOR EACH ROW EXECUTE PROCEDURE insert_project_post_process();
+FOR EACH ROW
+    EXECUTE FUNCTION insert_project_post_process();
 
 CREATE OR REPLACE TRIGGER set_ltree_path
 BEFORE INSERT OR UPDATE ON shadow_sources
